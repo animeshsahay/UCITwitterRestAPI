ffece4b88e80e2d671cc9d06f94c8ad16d62a2d1
animeshsahay <animeshsahay96@gmail.com>
2016-07-01 15:53:21 -0700

    TweetsSearch

---
+++ src/ChennaiTweetsJson.java
@@ -0,0 +1,5 @@
+/**
+ * Created by animesh on 7/1/16.
+ */
+public class ChennaiTweetsJson {
+}
--- src/TweetsSearch.java
+++ src/TweetsSearch.java
@@ -1,4 +1,5 @@
 import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
 
 import java.util.List;
 
@@ -7,11 +8,24 @@ import java.util.List;
  */
 public class TweetsSearch {
     public static void main(String[] args) {
+        //Authentication
+        ConfigurationBuilder cf = new ConfigurationBuilder();
+
+        cf.setDebugEnabled(true);
+        cf.setOAuthConsumerKey("SQzo1gMENQslwAJyEeszWCJJY");
+        cf.setOAuthConsumerSecret("2KGMg5OKki3YFDtlEn4C6Lpbe8CvXG0xoCkzSVnAf2Twnj3Wtv");
+        cf.setOAuthAccessToken("149596430-r5qAiFQMNXJ9mpFB9aNvOaB2RYxE3uronLZ1VbzG");
+        cf.setOAuthAccessTokenSecret("W0HliVCmfzoErfxwssRabQ7xAly4rgY5htnm4fVPOWnx1");
+
+        TwitterFactory tf = new TwitterFactory(cf.build());
+        Twitter twitter = tf.getInstance();
+
+        //Actual tweet scrapping
         if (args.length < 1) {
             System.out.println("java twitter4j.examples.search.SearchTweets [query]");
             System.exit(-1);
         }
-        Twitter twitter = new TwitterFactory().getInstance();
+        //Twitter twitter = new TwitterFactory().getInstance();
         try {
             Query query = new Query(args[0]);
             QueryResult result;
